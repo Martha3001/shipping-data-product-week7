4236c196641e2b0ecc536e8fb5234208894338c8
Martha <marthaket30@gmail.com>
2025-08-01 15:33:53 +0300

    update: add schedule

--- telegram_pipeline/pipelines/telegram_pipeline.py
+++ telegram_pipeline/pipelines/telegram_pipeline.py
@@ -1,42 +1,49 @@
-from dagster import op
-from dagster import job
-from dagster import Definitions
+from dagster import op, job, Definitions, ScheduleDefinition
 import subprocess
+import sys
+python_executable = sys.executable
 
 @op
 def scrape_telegram_data():
-    subprocess.run(["python", "scripts/data_scraping.py"], check=True)
+    subprocess.run([python_executable, r".\scripts\data_scraping.py"], check=True)
     return "done"
 
 @op
+def run_yolo_enrichment(_dbt_output):
+    subprocess.run([python_executable, r".\scripts\image_detection.py"], check=True)
+    return "yolo enriched"
+
+@op
 def load_raw_to_postgres(_scrape_output):
-    subprocess.run(["python", "scripts/load_raw_data.py"], check=True)
+    subprocess.run([python_executable, r".\scripts\load_raw_data.py"], check=True)
     return "loaded"
+
+@op
+def load_detected_object_to_postgres(_yolo_output):
+    subprocess.run([python_executable, r".\scripts\load_detected_objects.py"], check=True)
+    return "loaded detected"
 
 @op
 def run_dbt_transformations(_load_output):
     subprocess.run(["dbt", "run"], cwd="telegram_data", check=True)
     return "dbt run complete"
 
-@op
-def run_yolo_enrichment(_dbt_output):
-    subprocess.run(["python", "scripts\image_detection.py"], check=True)
-    return "yolo enriched"
-
-@op
-def load_detected_object_to_postgres(_yolo_output):
-    subprocess.run(["python", "scripts\load_detected_objects.py"], check=True)
-    return "loaded detected"
-
 @job
 def telegram_pipeline_job():
-    # define dependency order
+    # define order
     scrape = scrape_telegram_data()
-    load = load_raw_to_postgres(scrape)
-    dbt = run_dbt_transformations(load)
-    yolo = run_yolo_enrichment(dbt)
-    load_detected_object_to_postgres(yolo)
+    yolo = run_yolo_enrichment(scrape)
+    load = load_raw_to_postgres(yolo)
+    load_detect = load_detected_object_to_postgres(load)
+    run_dbt_transformations(load_detect)
+
+daily_pipeline_schedule = ScheduleDefinition(
+    job=telegram_pipeline_job,
+    cron_schedule="15 23 * * *",  
+    name="daily_data_pipeline_schedule"
+)
 
 defs = Definitions(
-    jobs=[telegram_pipeline_job]
+    jobs=[telegram_pipeline_job],
+    schedules=[daily_pipeline_schedule]
 )
